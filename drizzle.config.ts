804d01cb0cbba549a281fa818622a6bdb45be400
Tanay Vasishtha <tanayvasishtha@gmail.com>
2025-09-16 01:41:50 +0530

    fix: Update dependencies and refactor Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,8 @@
-import type { Config } from 'drizzle-kit/pg';
-
 export default {
     schema: './lib/db/schema.ts',
     out: './drizzle',
-    driver: 'pg',
+    dialect: 'postgresql',
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL!,
     },
-} satisfies Config;
+};
